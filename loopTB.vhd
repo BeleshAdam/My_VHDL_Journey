60a0194a6d4c15ed7aad0951344f86560655d637
Deniz İnce <deniz-ince35@outlook.com>
2025-12-03 17:46:23 +0300

    loop and if

--- loopTB.vhd
+++ loopTB.vhd
@@ -4,7 +4,8 @@ end entity;
 architecture sim of LoopTB is
 begin
 
-    process is
+    process
+        variable x : integer := 0;
     begin
 
         report  "Start!";
@@ -12,6 +13,10 @@ begin
         loop
             report "Boo";
             wait for 10 ns;
+            x := x + 1;
+            if x = 9 then -- stops at 10th boo
+                exit; -- exits the loop
+            end if;
         end loop;
 
         report "End!";
